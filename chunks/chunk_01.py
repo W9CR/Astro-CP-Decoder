74889fa152b177fb90080c144c22dd7c6598b264
Jason Kendall <coolacid@gmail.com>
2020-11-21 18:38:07 -0500

    Finish parsing chunk_01

--- chunks/chunk_01.py
+++ chunks/chunk_01.py
@@ -9,17 +9,16 @@ class chunk_01(chunkBasecalss):
             "26s",      # Unknown Data
             "L",        # Version
             "B",        # Model Type
-            "H",        # Unknown Nulls
-            "H",        # Pointer to Block 2
-            "H",        # Pointer to Black 7
+            "13H",      # Read 13 Pointers, Some may be Null, which we filter out
         ]
         self.description = CHUNK_TYPES[0x01]
         print(f"Parsing {self.description}")
         paserdData = self.unPack(format, data)
 
         # Get all the pointers, and shove them into the local pointer list
-        for pointers in [0, 5, 6]:
-            self.pointers.append(paserdData[pointers])
+        for pointer in [0, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]:
+            if paserdData[pointer] != 0x00:
+                self.pointers.append(paserdData[pointer])
 
         # Breakout the rest of the data
         self.data['version'] = paserdData[2]
